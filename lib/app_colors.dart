faa1d390d7f1e3ceae1c88a7c5af506b5fd02e85
happy jiwon <sjwon113@naver.com>
2024-11-24 02:00:14 +0900

    [ADD] 색상 추가

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -3,4 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primaryPink = Color(0xffff9094);
   static const Color primaryLightPink = Color(0xfff6c1c3);
+  static const Color primaryBackgroundColor = Colors.white;
+  static const Color dividerLineColor = Color(0x8EFFFFFF);
+  static const Color primaryDarkPink = Color(0xffd7565c);
 }
